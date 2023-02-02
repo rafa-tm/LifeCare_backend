61460f5313cfb5514340e8d4c548cbfa93804ccb
Rafael Tury Minatel <rafaelturyminatel@gmail.com>
2023-02-02 14:58:04 -0300

    inicio de login simulado

--- src/routes/LoginRoutes.ts
+++ src/routes/LoginRoutes.ts
@@ -1,8 +1,11 @@
+import { randomUUID } from "crypto";
 import { FastifyInstance } from "fastify";
 import { z } from "zod";
 import { prisma } from "../lib/prisma";
 
 export default async function LoginRoutes(app: FastifyInstance) {
+  const tokenAcess = randomUUID();
+
   //POST Login do sistema
   app.post("/login", async (request, reply) => {
     const criarProfissionalBody = z.object({
@@ -11,11 +14,28 @@ export default async function LoginRoutes(app: FastifyInstance) {
     });
     const { email, senha } = criarProfissionalBody.parse(request.body);
 
-    const cliente = await prisma.cliente.findFirstOrThrow({
+    const cliente = await prisma.cliente.findFirst({
       where: {
         email: email,
         senha: senha,
       },
     });
+
+    if (!cliente) {
+      const profissional = await prisma.profissional.findFirst({
+        where: {
+          email: email,
+          senha: senha,
+        },
+      });
+      if (!profissional) {
+        reply.code(401).send({ erro: "Usuário não cadastrado" });
+        return;
+      } else {
+        return { token_profissional: tokenAcess };
+      }
+    }
+
+    return { token_cliente: tokenAcess };
   });
 }
